1a12fa7febec3a668715acb47765cfbac3b84720
Riyan Setiyadi Hidayatullah <setiyariyan19@gmail.com>
2024-08-14 11:31:07 +0700

    Update list_auth_required_page.dart

--- lib/utils/list_auth_required_page.dart
+++ lib/utils/list_auth_required_page.dart
@@ -1,3 +1,8 @@
 import 'package:khm_app/utils/enum_app_page.dart';
 
-final List<AppPage> authRequiredPages = [AppPage.profile];
+final List<AppPage> authRequiredPages = [
+  AppPage.profile,
+  AppPage.chat,
+  AppPage.cart,
+  AppPage.riwayat
+];
