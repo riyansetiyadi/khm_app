255982e1fdc6a57db1c051e6f34677d45982f339
Riyan Setiyadi Hidayatullah <setiyariyan19@gmail.com>
2024-08-14 11:26:01 +0700

    update error widget

--- lib/widgets/handle_error_refresh_widget.dart
+++ lib/widgets/handle_error_refresh_widget.dart
@@ -5,17 +5,18 @@ import 'package:flutter/material.dart';
 class ErrorRefresh extends StatelessWidget {
   const ErrorRefresh({
     super.key,
-    required this.errorTitle,
+    this.errorTitle,
     this.refreshTitle,
     this.onPressed,
   });
 
-  final String errorTitle;
+  final String? errorTitle;
   final String? refreshTitle;
   final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
+    print(onPressed);
     return Center(
       child: Material(
         child: Padding(
@@ -24,7 +25,7 @@ class ErrorRefresh extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                errorTitle,
+                errorTitle ?? "Gagal mendapatkan data!",
                 style: Theme.of(context).textTheme.bodyMedium,
                 textAlign: TextAlign.center,
               ),
@@ -36,18 +37,33 @@ class ErrorRefresh extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
               if (onPressed != null)
-                IconButton(
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Warna latar belakang
+                    shape: BoxShape.circle, // Bentuk lingkaran
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(3, 3),
+                      ),
+                    ],
+                  ),
+                  child: IconButton(
                     onPressed: onPressed,
                     icon: Icon(
                       defaultTargetPlatform == TargetPlatform.iOS
                           ? CupertinoIcons.refresh
                           : Icons.refresh,
-                      color: Colors.white,
+                      color: Colors.green,
                       size: 20,
                     ),
                     style: ButtonStyle(
                       iconSize: WidgetStateProperty.all<double>(35),
-                    )),
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
