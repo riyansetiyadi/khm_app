5108226dd777f5ef4607d69eca41d3207dc6c71f
Riyan Setiyadi Hidayatullah <setiyariyan19@gmail.com>
2024-09-04 23:35:31 +0700

    add permission handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:khm_app/db/auth_repository.dart';
 import 'package:khm_app/provider/address_provider.dart';
 import 'package:khm_app/provider/auth_provider.dart';
@@ -6,13 +7,16 @@ import 'package:khm_app/provider/cart_provider.dart';
 import 'package:khm_app/provider/chat_provider.dart';
 import 'package:khm_app/provider/transaction_provider.dart';
 import 'package:khm_app/provider/product_provider.dart';
-import 'package:khm_app/routes/page_manager.dart';
-import 'package:khm_app/routes/route_information_parser.dart';
 import 'package:khm_app/service/api_service.dart';
 import 'package:provider/provider.dart';
 import 'package:khm_app/routes/config.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('id_ID', null);
+
   runApp(const KhmApp());
 }
 
@@ -30,7 +34,6 @@ class _StoryAppState extends State<KhmApp> {
   late AddressProvider addressProvider;
   late TransactionProvider historyTransactionProvider;
   late ChatProvider chatProvider;
-  late AppRouteInformationParser routeInformationParser;
 
   @override
   void initState() {
@@ -63,16 +66,13 @@ class _StoryAppState extends State<KhmApp> {
       authRepository,
     );
 
-    routeInformationParser = AppRouteInformationParser();
+    requestPermissions();
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (context) => PageManager(),
-        ),
         ChangeNotifierProvider(
           create: (context) => authProvider,
         ),
@@ -96,5 +96,20 @@ class _StoryAppState extends State<KhmApp> {
         routerConfig: router,
       ),
     );
+  }
+
+  Future<void> requestPermissions() async {
+    // Meminta izin penyimpanan
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      // Jika izin belum diberikan, minta izin
+      if (await Permission.storage.request().isGranted) {
+        print("Izin penyimpanan diberikan.");
+      } else {
+        print("Izin penyimpanan ditolak.");
+      }
+    } else {
+      print("Izin penyimpanan sudah diberikan.");
+    }
   }
 }
