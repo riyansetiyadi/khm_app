8d8f81d352c13accc751a4f84217936e4687a3b0
Riyan Setiyadi Hidayatullah <setiyariyan19@gmail.com>
2024-09-25 15:39:58 +0700

    refactoring
    - memisahkan init fcm dan webview
    - add background loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
 import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:khm_app/models/notificationModel.dart';
+import 'package:khm_app/utils/fcm_helper.dart';
 import 'package:khm_app/utils/notification_helper.dart';
-import 'package:url_launcher/url_launcher.dart';
-
+import 'package:khm_app/utils/webview_helper.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// Import for Android features.
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-// Import for iOS/macOS features.
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -24,6 +15,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final NotificationHelper notificationHelper = NotificationHelper();
+  final FcmHelper fcmHelper = FcmHelper();
 
   String? selectedNotificationPayload;
 
@@ -40,19 +32,8 @@ Future<void> main() async {
   }
   await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await FirebaseMessaging.instance.requestPermission();
-  await FirebaseMessaging.instance.getToken().then((token) {
-    print('FCM Token: $token');
-  });
+  await fcmHelper.initFcm(flutterLocalNotificationsPlugin);
 
-  FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
   runApp(const KhmApp());
 }
 
@@ -64,6 +45,7 @@ class KhmApp extends StatefulWidget {
 }
 
 class _KhmAppState extends State<KhmApp> {
+  final WebviewHelper webviewHelper = WebviewHelper();
   late final WebViewController _controller;
   bool _isLoading = false;
   String mainUrl = 'https://simkhm.id/';
@@ -72,109 +54,14 @@ class _KhmAppState extends State<KhmApp> {
   void initState() {
     super.initState();
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${int.tryParse(message.data['id'])}');
-      print('Message data: ${message.notification?.title}');
-      print('Message data: ${message.notification?.body}');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-      final NotificationHelper notificationHelper = NotificationHelper();
-      NotificationModel restaurant = NotificationModel(
-          id: int.tryParse(message.data['id']) ?? 0,
-          title: message.notification?.title,
-          body: message.notification?.body,
-          payload: 'payload');
-      await notificationHelper.showNotification(
-          flutterLocalNotificationsPlugin, restaurant);
-    });
-
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    final WebViewController controller =
-        WebViewController.fromPlatformCreationParams(params);
-    // #enddocregion platform_features
-
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              _isLoading = true;
-            });
-          },
-          onPageFinished: (String url) {
-            setState(() {
-              _isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-              Page resource error:
-                code: ${error.errorCode}
-                description: ${error.description}
-                errorType: ${error.errorType}
-                isForMainFrame: ${error.isForMainFrame}
-            ''');
-          },
-          onNavigationRequest: (NavigationRequest request) async {
-            final Uri url = Uri.parse(request.url);
-            final String currentHost = Uri.parse('https://simkhm.id/').host;
-
-            if (url.host != currentHost) {
-              if (await canLaunchUrl(url)) {
-                await launchUrl(url, mode: LaunchMode.externalApplication);
-              } else {
-                await launchUrl(url, mode: LaunchMode.inAppWebView);
-              }
-              return NavigationDecision.prevent;
-            } else {
-              return NavigationDecision.navigate;
-            }
-          },
-          onHttpError: (HttpResponseError error) {
-            debugPrint('Error occurred on page: ${error.response?.statusCode}');
-          },
-          onUrlChange: (UrlChange change) {
-            debugPrint('url change to ${change.url}');
-          },
-        ),
-      )
-      ..addJavaScriptChannel(
-        'Toaster',
-        onMessageReceived: (JavaScriptMessage message) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        },
-      )
-      ..loadRequest(Uri.parse(mainUrl));
-
-    // setBackgroundColor is not currently supported on macOS.
-    if (kIsWeb || !Platform.isMacOS) {
-      controller.setBackgroundColor(const Color(0x80000000));
-    }
-
-    // #docregion platform_features
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-    }
-    // #enddocregion platform_features
-
-    _controller = controller;
+    _controller = webviewHelper.initWebview(
+      mainUrl: mainUrl,
+      onLoadingChanged: (isLoading) {
+        setState(() {
+          _isLoading = isLoading;
+        });
+      },
+    );
   }
 
   @override
@@ -202,9 +89,12 @@ class _KhmAppState extends State<KhmApp> {
               ),
             ),
             if (_isLoading)
-              Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                child: Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                  ),
                 ),
               ),
           ],
