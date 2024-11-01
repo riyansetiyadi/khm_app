0f8c20375fe902791157fe5ce3f86f671cdfa4ff
Jessica <jessica.amanda1410@gmail.com>
2024-11-01 10:54:57 +0700

    update home

--- lib/screen/simkhm/home_screen.dart
+++ lib/screen/simkhm/home_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:khm_app/widgets/drawer/main_drawer.dart';
-import 'package:khm_app/widgets/navigation_bar/main_app_bar.dart';
 import 'package:khm_app/widgets/navigation_bar/main_bottom_bar.dart';
-import 'package:khm_app/widgets/drawer/main_drawer.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeSimkhmScreen extends StatefulWidget {
   const HomeSimkhmScreen({super.key});
@@ -13,32 +12,18 @@ class HomeSimkhmScreen extends StatefulWidget {
 }
 
 class _HomeSimkhmScreenState extends State<HomeSimkhmScreen> {
-  // final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  // late WebViewController _controller;
-int _selectedIndex = 0; 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        // key: _scaffoldKey,
-        // appBar: AppBar(
-        //   title: Text('SIMKHM'),
-        //   leading: IconButton(
-        //     icon: Icon(Icons.menu),
-        //     onPressed: () {
-        //       _scaffoldKey.currentState?.openDrawer();
-        //     },
-        //   ),
-        // ),
-        // drawer: MainDrawer(
-        //   scaffoldKey: _scaffoldKey,
-        //   controller: _controller,
-        // ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('SIMKHM'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -48,9 +33,8 @@ int _selectedIndex = 0;
                 borderRadius: BorderRadius.circular(12.0),
               ),
               child: Image(
-                  image: NetworkImage(
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOuPYK2oy_VajJDRE_Y4S1qgC9WdLTVHmCbg&s'),
-                  height: 200,
+                  image: AssetImage('assets/images/benner.png'),
+                  height: 150,
                   width: double.infinity,
                   fit: BoxFit.cover),
             ),
@@ -61,8 +45,8 @@ int _selectedIndex = 0;
             ),
             SizedBox(height: 20),
             Wrap(
-              spacing: 10,
-              runSpacing: 10,
+              spacing: 15,
+              runSpacing: 15,
               children: [
                 InkWell(
                   onTap: () {},
@@ -135,7 +119,8 @@ int _selectedIndex = 0;
             Container(
               padding: EdgeInsets.all(12.0),
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Expanded(
                     flex: 1,
@@ -151,18 +136,21 @@ int _selectedIndex = 0;
                       children: [
                         const Text(
                           'Yuk, Segera Belanja Kosmestik Sekarang!',
-                          style: TextStyle(fontSize: 14),
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                         const SizedBox(height: 5),
                         ElevatedButton(
                           onPressed: () {},
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.yellow,
+                            backgroundColor: const Color.fromARGB(255, 240, 218, 22),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(30),
                             ),
                           ),
-                          child: const Text('Klik di sini ya!'),
+                          child: const Text('Klik di sini ya!', style: TextStyle(color: Colors.black),),
                         ),
                       ],
                     ),
@@ -171,111 +159,109 @@ int _selectedIndex = 0;
               ),
             ),
             SizedBox(height: 18),
-            Container(
-              color: Color(0XFF08592f),
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hubungi Kami',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.white),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Text(
-                        'KHM Wonorejo : ',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.whatsapp),
-                          color: Colors.white,
-                          onPressed: () {}),
-                      Text('CS +62 822 3388 0001',
-                          style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                  SizedBox(height: 0),
-                  Row(
-                    children: [
-                      Text(
-                        'KHM Klakah : ',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.whatsapp),
-                          color: Colors.white,
-                          onPressed: () {}),
-                      Text('CS +62 813 5555 0275',
-                          style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'KHM Tunjung :',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.whatsapp),
-                          color: Colors.white,
-                          onPressed: () {}),
-                      Text('CS +62 812 3457 1010',
-                          style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    'Ikuti Kami',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.white),
-                  ),
-                  SizedBox(height: 12),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.instagram),
-                          color: Colors.white,
-                          onPressed: () {}),
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.facebook),
-                          color: Colors.white,
-                          onPressed: () {}),
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.tiktok),
-                          color: Colors.white,
-                          onPressed: () {}),
-                      IconButton(
-                          icon: FaIcon(FontAwesomeIcons.youtube),
-                          color: Colors.white,
-                          onPressed: () {}),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Image(
-                    image: NetworkImage(
-                        'https://simkhm.id/assets/img/20230831_110803.png'),
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.cover,
-                  ),
-                ],
+              Container(
+                color: Color(0XFF08592f),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hubungi Kami',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white),
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Text(
+                          'KHM Wonorejo : ',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.whatsapp),
+                            color: Colors.white,
+                            onPressed: () {}),
+                        Text('CS +62 822 3388 0001',
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                    SizedBox(height: 0),
+                    Row(
+                      children: [
+                        Text(
+                          'KHM Klakah : ',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.whatsapp),
+                            color: Colors.white,
+                            onPressed: () {}),
+                        Text('CS +62 813 5555 0275',
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'KHM Tunjung :',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.whatsapp),
+                            color: Colors.white,
+                            onPressed: () {}),
+                        Text('CS +62 812 3457 1010',
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Ikuti Kami',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white),
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.instagram),
+                            color: Colors.white,
+                            onPressed: () {}),
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.facebook),
+                            color: Colors.white,
+                            onPressed: () {}),
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.tiktok),
+                            color: Colors.white,
+                            onPressed: () {}),
+                        IconButton(
+                            icon: FaIcon(FontAwesomeIcons.youtube),
+                            color: Colors.white,
+                            onPressed: () {}),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Image(
+                      image: NetworkImage(
+                          'https://simkhm.id/assets/img/20230831_110803.png'),
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
               ),
-              
-            ),
-          ],
+            ],
         ),
       ),
-       bottomNavigationBar: MainBottomBar(
-        initiateIndex: _selectedIndex, // Mengoper indeks yang dipilih
-        onTap: _onItemTapped,   // Mengoper fungsi untuk menangani ketukan
+      bottomNavigationBar: MainBottomBar(
+        initiateIndex: 0,
       ),
     ));
   }
